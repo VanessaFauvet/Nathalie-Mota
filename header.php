a8ba1230df856ad13302a4d1d2efe774e60f7a77
VanessaF <89605309+VanessaFauvet@users.noreply.github.com>
2024-07-31 01:04:58 +0200

    Media queries

--- header.php
+++ header.php
@@ -10,13 +10,13 @@
 
 <body <?php body_class(); ?>>
     <header>
-        <nav>
+        <div class="headerContainer">
             <div class="logo">
                 <a href="<?php echo home_url( '/' ); ?>">
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="Logo">
                 </a>
             </div>
-        </nav>
+        </div>
 
         <div class="burger">
             <span class="bar"></span>
@@ -24,11 +24,11 @@
             <span class="bar"></span>
         </div>
             
-        <div class="main-nav">
+        <nav class="main-nav">
             <?php wp_nav_menu(array(
                 'theme_location' => 'main',
                 'container' => 'ul', // afin d'éviter d'avoir une div autour
                 )); 
             ?>
-        </div>
+        </nav>
     </header>
--- template-parts/lightbox.php
+++ template-parts/lightbox.php
@@ -8,25 +8,26 @@ $photo = get_the_post_thumbnail_url(null, 'full');
     <div id="lightboxWrapper" class="lightboxWrapper">
         <!-- Div pour le contenu de la lightbox -->
         <div class="lightboxContent">
-            <!-- Section pour afficher la catégorie de la photo -->
-            <span class="lightboxCategory"></span>
-
-            <!-- Section pour afficher la référence de la photo -->
-            <span class="lightboxReference"></span>
             
             <!-- Croix pour fermer la lightbox -->
             <img class="closeLightbox" src="<?php echo get_stylesheet_directory_uri() . '/assets/img/white_cross.png'; ?>" alt="fermeture de la lightbox">
-
-            <!-- Flèche pour la navigation vers la photo précédente -->
-            <span><img class="lightboxPrevious" src="<?php echo get_stylesheet_directory_uri() . '/assets/img/previous.png'; ?>" alt="navigation précedente"></span>
             
             <!-- Div pour afficher la photo -->
             <div class="lightboxImageContainer">
                 <img src="<?php echo $photo; ?>" class="lightboxImage" alt="">
             </div>
+
+            <!-- Flèche pour la navigation vers la photo précédente -->
+            <span><img class="lightboxPrevious" src="<?php echo get_stylesheet_directory_uri() . '/assets/img/previous.png'; ?>" alt="navigation précedente"></span>
             
             <!-- Flèche pour la navigation vers la photo suivante -->
             <span><img class="lightboxNext" src="<?php echo get_stylesheet_directory_uri() . '/assets/img/next.png'; ?>" alt="navigation suivante"></span>
+
+            <!-- Section pour afficher la référence de la photo -->
+            <span class="lightboxReference"></span>
+
+            <!-- Section pour afficher la catégorie de la photo -->
+            <span class="lightboxCategory"></span>
         </div>
     </div>
 </div>
